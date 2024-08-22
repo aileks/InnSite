caed13d0c6e08122c617029e82802d3a4cb2d688
Aaliyah Harper <aeverly14@pm.me>
2024-08-22 19:08:43 -0400

    added page not found route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,6 +32,31 @@ const router = createBrowserRouter([
           </div>
         ),
       },
+      {
+        path: '*',
+        element: (
+          <div
+            style={{
+              marginTop: '20vh',
+              width: '100vw',
+            }}
+          >
+            <h1 style={{ textAlign: 'center', margin: '10px 0' }}>
+              Something went wrong...
+            </h1>
+
+            <h2 style={{ textAlign: 'center' }}>
+              Page not found
+              <br />
+              <img
+                style={{ marginTop: '10px' }}
+                src='https://media1.tenor.com/m/KfTzPr3nyowAAAAd/what-que.gif'
+                alt=''
+              />
+            </h2>
+          </div>
+        ),
+      },
     ],
   },
 ]);
