9f23ea8fa1d7f872286f01f82edc888c91677a54
Aaliyah Harper <aeverly14@pm.me>
2024-08-22 18:45:52 -0400

    deploy

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,37 +11,32 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
   });
 
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve('../frontend/build')));
+  router.use(express.static(path.resolve('../frontend/dist')));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
-      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
   });
 }
 
+// Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    const csrfToken = req.csrfToken();
+    res.cookie('XSRF-TOKEN', csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken,
+    });
   });
 }
-
-// Add a XSRF-TOKEN cookie
-// router.get('/api/csrf/restore', (req, res) => {
-//   const csrfToken = req.csrfToken();
-//   res.cookie('XSRF-TOKEN', csrfToken);
-//   res.status(200).json({
-//     'XSRF-Token': csrfToken,
-//   });
-// });
 
 module.exports = router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,27 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <h1 style={{ padding: '8px 10px' }}>Welcome!</h1>,
+        element: (
+          <div
+            style={{
+              marginTop: '20vh',
+            }}
+          >
+            <h1 style={{ textAlign: 'center', marginBottom: '10px' }}>
+              Welcome!
+            </h1>
+
+            <h2 style={{ textAlign: 'center', marginTop: '10px' }}>
+              We&apos;re still under construction...
+              <br />
+              <img
+                style={{ marginTop: '10px' }}
+                src='https://media1.tenor.com/m/XPRG-4ujVMIAAAAd/cat-work-in-progress.gif'
+                alt=''
+              />
+            </h2>
+          </div>
+        ),
       },
     ],
   },
