3163a76777ee41af002b034afd479117d2999164
Aaliyah Harper <aeverly14@pm.me>
2024-08-21 20:31:31 -0400

    added profile dropdown menu

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,35 +1,16 @@
 import './Navigation.css';
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import { logout } from '../../store/session';
 
 export default function Navigation({ isLoaded }) {
   const user = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
 
-  const destroy = e => {
-    e.preventDefault();
-
-    dispatch(logout());
-  };
-
-  const sessionLinks =
+  const navLinks =
     user ?
-      <>
-        <li className='nav-link'>
-          <ProfileButton user={user} />
-        </li>
-
-        <li className='nav-link'>
-          <button
-            id='nav-logout'
-            onClick={destroy}
-          >
-            Log Out
-          </button>
-        </li>
-      </>
+      <li className='nav-link'>
+        <ProfileButton user={user} />
+      </li>
     : <>
         <li className='nav-link'>
           <NavLink to='/login'>Log In</NavLink>
@@ -42,11 +23,11 @@ export default function Navigation({ isLoaded }) {
 
   return (
     <ul id='nav'>
-      <li className='nav-link' id='home-link'>
+      <li id='home-link' className='nav-link'>
         <NavLink to='/'>Home</NavLink>
       </li>
 
-      {isLoaded && sessionLinks}
+      {isLoaded && navLinks}
     </ul>
   );
 }
--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,17 +1,63 @@
-import { IoPersonCircleSharp } from 'react-icons/io5';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { LuUserCircle } from 'react-icons/lu';
+import { logout } from '../../store/session';
 
-const PersonIcon = () => {
-  return (
-    <div style={{ fontSize: '2.2em', marginTop: '8px', cursor: 'pointer' }}>
-      <IoPersonCircleSharp />
-    </div>
-  );
-};
+export default function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
 
-export default function ProfileButton() {
+  const ulClassName = 'profile-dropdown' + (showMenu ? '' : ' hidden');
+
+  const destroy = e => {
+    e.preventDefault();
+    dispatch(logout());
+  };
+
+  const toggleMenu = e => {
+    e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener('click', closeMenu);
+  }, [showMenu]);
+
   return (
     <>
-      <PersonIcon />
+      <button
+        style={{ color: 'black', fontSize: '2.2rem' }}
+        onClick={toggleMenu}
+      >
+        <LuUserCircle />
+      </button>
+
+      <ul className={ulClassName}>
+        <li>{user.username}</li>
+
+        <li>
+          {user.firstName} {user.lastName}
+        </li>
+
+        <li>{user.email}</li>
+
+        <li>
+          <button
+            id='nav-logout'
+            onClick={destroy}
+          >
+            Log Out
+          </button>
+        </li>
+      </ul>
     </>
   );
 }
