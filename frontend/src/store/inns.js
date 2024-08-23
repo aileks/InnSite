f8298ced4ad08679a55a4f9186992bfe032f095f
Aaliyah Harper <aeverly14@pm.me>
2024-08-23 14:58:35 -0400

    add singleInn thunk

--- frontend/src/store/inns.js
+++ frontend/src/store/inns.js
@@ -1,11 +1,19 @@
 import { csrfFetch } from './csrf';
 
 const LOAD_INNS = 'inns/loadInns';
+const SINGLE_INN = 'inns/singleInn';
 
 export const loadInns = inns => {
   return {
     type: LOAD_INNS,
     inns,
+  };
+};
+
+export const singleInn = inn => {
+  return {
+    type: SINGLE_INN,
+    inn,
   };
 };
 
@@ -15,6 +23,17 @@ export const getAllInns = () => async dispatch => {
   if (res.ok) {
     const data = await res.json();
     dispatch(loadInns(data.Spots));
+  }
+
+  return res;
+};
+
+export const getInnById = innId => async dispatch => {
+  const res = await csrfFetch(`/api/spots/${innId}`);
+
+  if (res.ok) {
+    const inn = await res.json();
+    dispatch(singleInn(inn))
   }
 
   return res;
