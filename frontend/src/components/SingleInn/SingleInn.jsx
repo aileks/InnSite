eb99c6ab713a7cb5139451bf0e37ff20506f891f
Aaliyah Harper <aeverly14@pm.me>
2024-08-23 16:13:02 -0400

    fixed undefined check on single inn

--- frontend/src/components/SingleInn/SingleInn.jsx
+++ frontend/src/components/SingleInn/SingleInn.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { selectInnById, getInnById } from '../../store/inns';
 import { useEffect } from 'react';
+import NotFound from '../404';
 
 export default function SingleInn() {
   const dispatch = useDispatch();
@@ -11,19 +12,23 @@ export default function SingleInn() {
 
   useEffect(() => {
     dispatch(getInnById(id));
-  }, [id, dispatch])
+  }, [id, dispatch]);
 
   return (
-    <div id='inn-container'>
-      <h1 id='inn-header'>{inn?.name}</h1>
+    <>
+      {inn ?
+        <div id='inn-container'>
+          <h1 id='inn-header'>{inn.name}</h1>
 
-      <h2 id='inn-description'>{inn?.description}</h2>
+          <h2 id='inn-description'>{inn.description}</h2>
 
-      <div id='inn-price'>${inn?.price} per night</div>
+          <div id='inn-price'>${inn.price} per night</div>
 
-      <div id='inn-location'>
-        Located in {inn?.city}, {inn?.state}
-      </div>
-    </div>
+          <div id='inn-location'>
+            Located in {inn.city}, {inn.state}
+          </div>
+        </div>
+      : <NotFound />}
+    </>
   );
 }
