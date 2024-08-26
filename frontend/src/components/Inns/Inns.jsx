66c44850d5d69c58dcfec7b4418d67086d0faa49
aileks <aeverly14@pm.me>
2024-08-26 15:54:25 -0400

    added missing data to landing page

--- frontend/src/components/Inns/Inns.jsx
+++ frontend/src/components/Inns/Inns.jsx
@@ -3,10 +3,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
 import { getAllInns, selectInnsArray } from '../../store/inns';
+import { SlMagicWand } from 'react-icons/sl';
 
 export default function Inns() {
   const dispatch = useDispatch();
   const inns = useSelector(selectInnsArray);
+
+  console.log(inns);
 
   useEffect(() => {
     dispatch(getAllInns());
@@ -16,7 +19,23 @@ export default function Inns() {
     <ul>
       {inns?.map(inn => (
         <li key={inn.id}>
-          <Link to={`inns/${inn.id}`}>{inn.name}</Link>
+          <Link to={`inns/${inn.id}`}>
+            <img
+              className='landing-preview'
+              src={inn.previewImage}
+              alt=''
+            />
+          </Link>
+
+          <div className='inn-info'>
+            <Link to={`inns/${inn.id}`}>{inn.name}</Link>
+            {' • '}
+            {inn.city}, {inn.state}
+            {' • '}
+            {inn.avgRating ? inn.avgRating.toFixed(2) : 'New'} <SlMagicWand style={{ color: '#6a0dad', fontSize: '0.9em' }} />
+            {' • '}
+            ${inn.price}/night
+          </div>
         </li>
       ))}
     </ul>
