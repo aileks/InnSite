d9929dab1169c79e04a1ff92ed6926917472802b
aileks <aeverly14@pm.me>
2024-08-26 15:29:31 -0400

    change STRING datatype to TEXT

--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       reviewId: DataTypes.INTEGER,
       url: {
-        type: DataTypes.TEXT,
+        type: DataTypes.STRING,
         allowNull: false,
         validate: {
           notNull: {
