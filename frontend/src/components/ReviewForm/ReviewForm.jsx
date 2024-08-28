dd66405bef4186de42133b8b96dc553f88a136d7
aileks <aeverly14@pm.me>
2024-08-28 11:00:47 -0400

    test commit

--- frontend/src/components/ReviewForm/ReviewForm.jsx
+++ frontend/src/components/ReviewForm/ReviewForm.jsx
@@ -1,11 +1,12 @@
 import './ReviewForm.css';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+// import { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { SlMagicWand } from 'react-icons/sl';
 
 export default function ReviewForm() {
-  const dispatch = useDispatch();
-  const user = useSelector(state => state.session.user);
-  const [reviewText, setReviewText] = useState();
+  // const dispatch = useDispatch();
+  // const user = useSelector(state => state.session.user);
+  // const [reviewText, setReviewText] = useState();
 
   return (
     <div>
