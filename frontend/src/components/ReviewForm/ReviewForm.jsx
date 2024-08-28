d2d85811f91971e46cc66b1f6fdaba8a4c5826f0
aileks <aeverly14@pm.me>
2024-08-28 11:10:11 -0400

    on pause

--- frontend/src/components/ReviewForm/ReviewForm.jsx
+++ frontend/src/components/ReviewForm/ReviewForm.jsx
@@ -1,15 +1,14 @@
 import './ReviewForm.css';
-// import { useState } from 'react';
-import { useSelector } from 'react-redux';
+// import { useState, useEffect } from 'react';
+// import { useSelector } from 'react-redux';
 
 export default function ReviewForm() {
-  const user = useSelector(state => state.session.user);
-
-  console.log(user);
+  // const user = useSelector(state => state.session.user);
+  // const [reviewText, setReviewText] = useState();
 
   return (
     <div>
-      <form action=""></form>
+      <h1>Hello from ReviewForm</h1>
     </div>
   )
 }
