0e3e5632690e21ff44834114e4385bad6c5059bb
aileks <aeverly14@pm.me>
2024-08-28 19:06:54 -0400

    created toast

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { Provider } from "react-redux";
-import configureStore from "./store";
-import { restoreCSRF, csrfFetch } from "./store/csrf";
-import * as sessionActions from "./store/session";
-import ModalProvider, { Modal } from "./context/Modal";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+import ModalProvider, { Modal } from './context/Modal';
+import { ToastProvider } from './context/Toast';
 
 const store = configureStore();
 
-if (import.meta.env.MODE !== "production") {
+if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
@@ -18,13 +19,15 @@ if (import.meta.env.MODE !== "production") {
   window.sessionActions = sessionActions;
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ModalProvider>
-      <Provider store={store}>
-        <App />
-        <Modal />
-      </Provider>
-    </ModalProvider>
+    <Provider store={store}>
+      <ModalProvider>
+        <ToastProvider>
+          <App />
+          <Modal />
+        </ToastProvider>
+      </ModalProvider>
+    </Provider>
   </React.StrictMode>,
 );
