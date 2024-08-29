18df3711b9f59605f5071b66561908784f0c4754
aileks <aeverly14@pm.me>
2024-08-29 18:17:05 -0400

    schema update

--- backend/db/migrations/20240731215251-create-spot.js
+++ backend/db/migrations/20240731215251-create-spot.js
@@ -54,7 +54,7 @@ module.exports = {
           allowNull: false,
         },
         description: {
-          type: Sequelize.STRING,
+          type: Sequelize.TEXT,
           allowNull: false,
         },
         price: {
--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -115,7 +115,7 @@ module.exports = (sequelize, DataTypes) => {
         },
       },
       description: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false,
         validate: {
           notNull: {
