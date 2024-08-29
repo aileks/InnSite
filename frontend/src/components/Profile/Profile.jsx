5f8a6dcaf1cb74983a0dd4f400883fd60872caf5
aileks <aeverly14@pm.me>
2024-08-29 08:45:49 -0400

    fixed create inn button on profile

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,16 +1,16 @@
-import "./Profile.css";
-import { selectInnsArray, getUserInns } from "../../store/inns";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import InnCard from "../Inns/InnCard";
+import './Profile.css';
+import { selectInnsArray, getUserInns } from '../../store/inns';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import InnCard from '../Inns/InnCard';
 
 export default function Profile() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const user = useSelector((state) => state.session.user);
+  const user = useSelector(state => state.session.user);
   const allInns = useSelector(selectInnsArray);
-  const userInns = allInns?.filter((inn) => inn.ownerId === user?.id);
+  const userInns = allInns?.filter(inn => inn.ownerId === user?.id);
 
   useEffect(() => {
     dispatch(getUserInns());
@@ -26,20 +26,27 @@ export default function Profile() {
     <>
       {userInns.length ? (
         <>
-          <h1 id="profile-header">Manage Inns</h1>
-          <div className="inns-grid">
-            {userInns.map((inn) => (
-              <InnCard key={inn.id} inn={inn} />
+          <h1 id='profile-header'>Manage Inns</h1>
+          <div className='inns-grid'>
+            {userInns.map(inn => (
+              <InnCard
+                key={inn.id}
+                inn={inn}
+              />
             ))}
           </div>
         </>
       ) : (
-        <div id="profile-container">
+        <div id='profile-container'>
           <h2>You don&apos;t have any inns yet!</h2>
 
-          <button className="create-inn-button" id="profile-create-inn">
-            <Link to="/inns/new">Create a New Inn</Link>
-          </button>
+          <Link
+            className='create-inn-button'
+            id='profile-create-inn'
+            to='/inns/new'
+          >
+            Create a New Inn
+          </Link>
         </div>
       )}
     </>
