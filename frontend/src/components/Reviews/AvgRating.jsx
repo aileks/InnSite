ac7a2438c411b9e9f030538d145acb198c0e2562
aileks <aeverly14@pm.me>
2024-08-29 19:13:42 -0400

    build & deploy

--- frontend/src/components/Reviews/AvgRating.jsx
+++ frontend/src/components/Reviews/AvgRating.jsx
@@ -1,20 +1,22 @@
-import { SlMagicWand } from 'react-icons/sl';
+import { SlMagicWand } from "react-icons/sl";
 
 export default function AvgRating({ inn }) {
-  const inn = useSelector(state => selectInnById(state, inn.id));
-
-  const { avgStarRating, numReviews } = inn;
+  const { avgStarRating, numReviews } = inn || null;
 
   return (
-    <div id='avg-rating'>
+    <div id="avg-rating">
       {avgStarRating && <>Rating: {avgStarRating.toFixed(2)}</>}
 
-      <SlMagicWand style={{ color: '#6a0dad' }} />
+      <SlMagicWand style={{ color: "#6a0dad" }} />
 
       {numReviews ? (
-        <>{numReviews === 1 ? `• ${numReviews} Review` : `• ${numReviews} Reviews`} </>
+        <>
+          {numReviews === 1
+            ? `• ${numReviews} Review`
+            : `• ${numReviews} Reviews`}{" "}
+        </>
       ) : (
-        '*New*'
+        "*New*"
       )}
     </div>
   );
