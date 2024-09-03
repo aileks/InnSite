d67accaf4bc96c84600560a449558538b269025b
aileks <aeverly14@pm.me>
2024-09-03 09:12:01 -0400

    build & deploy

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,3 @@
-// import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-// import thunk from 'redux-thunk';
 import { configureStore } from '@reduxjs/toolkit';
 import sessionReducer from './sessionSlice';
 import innsReducer from './innsSlice';
@@ -12,17 +10,10 @@ const store = configureStore({
     inns: innsReducer,
     reviews: reviewsReducer,
   },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
-  devTools: process.env.NODE_ENV !== 'production',
+  middleware: getDefaultMiddleware =>
+    process.env.NODE_ENV !== 'production'
+      ? getDefaultMiddleware().concat(logger)
+      : getDefaultMiddleware(),
 });
-
-// let enhancer;
-// if (import.meta.env.MODE === 'production') {
-//   enhancer = applyMiddleware(thunk);
-// } else {
-//   const logger = (await import('redux-logger')).default;
-//   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-//   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-// }
 
 export default store;
